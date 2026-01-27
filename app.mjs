b8316a117ea9584481a2621f58e4290989378d97
VISHNU M K <mail.vishnumk@gmail.com>
2026-01-27 16:11:06 +0530

    added cron

--- app.mjs
+++ app.mjs
@@ -1,9 +1,27 @@
 import express from "express";
 import { db } from "./config/db.config.mjs";
 import "./bot/main.mjs";
+import nodeCron from "node-cron";
 
 await db.config();
 const app = express();
+
+app.get("/api/v1", (req, res) => {
+    return res.status(200).send({ 
+        ok: true,
+        message: "Everything is working fine."
+    });
+});
+
+nodeCron.schedule("* * * * *", async () => {
+    try {
+        const response = await fetch(`${process.env.SERVER}/api/v1`);
+        const data = await response.json();
+        return console.log(data);
+    } catch (err) {
+        return console.log(err.message);   
+    }
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on port ${process.env.PORT || 3000}`);
