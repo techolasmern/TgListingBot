d398313093c77f2358b4f707e4edbfc8d60c3b88
thenamevishnu <mail.vishnumk@gmail.com>
2026-03-05 13:47:27 +0530

    updated webhook api

--- app.mjs
+++ app.mjs
@@ -2,10 +2,16 @@ import express from "express";
 import { db } from "./config/db.config.mjs";
 import "./bot/main.mjs";
 import env from "dotenv"
+import { bot } from "./config/bot.config.mjs";
 env.config()
 
 await db.config();
 const app = express();
+
+app.get("/set-webhook", async (req, res) => {
+    await bot.api.setWebhook(process.env.SERVER);
+    res.send("Webhook set");
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on port ${process.env.PORT || 3000}`);
